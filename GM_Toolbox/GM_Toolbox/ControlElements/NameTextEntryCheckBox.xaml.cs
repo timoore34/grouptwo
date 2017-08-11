4a97d6796f5293a1c4f9df66efc89cc6ea6d4919
JNash7298 <JNash@student.neumont.edu>
2017-08-10 18:23:45 -0600

    Mostly User Control Elements added in.

---
+++ GM_Toolbox/GM_Toolbox/ControlElements/NameTextEntryCheckBox.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GM_Toolbox.ControlElements
+{
+    /// <summary>
+    /// This control gives you a checkbox that can be used to endable or disable use of control elements.
+    /// While providing three textbox fields for text entry.
+    /// </summary>
+    public partial class NameTextEntryCheckBox : UserControl
+    {
+        private int textBoxHeight;
+        private int textBoxFontSize;
+        private int labelFontSize;
+        private int firstTextBoxWidth;
+        private int secondTextBoxWidth;
+        private int thirdTextBoxWidth;
+        private int checkBoxSize;
+        private Brush labelColor;
+        private string checkBoxContent;
+        private string firstLabelContent;
+        private string secondLabelContent;
+        private string thirdLabelContent;
+        public int TextBoxHeight
+        {
+            get { return textBoxHeight; }
+            set
+            {
+                textBoxHeight = value;
+                FirstTextBox.Height = textBoxHeight;
+                SecondTextBox.Height = textBoxHeight;
+                ThirdTextBox.Height = textBoxHeight;
+            }
+        }
+        public int TextBoxFontSize
+        {
+            get { return textBoxFontSize; }
+            set
+            {
+                textBoxFontSize = value;
+                FirstTextBox.FontSize = textBoxFontSize;
+                SecondTextBox.FontSize = textBoxFontSize;
+                ThirdTextBox.FontSize = textBoxFontSize;
+            }
+        }
+        public int LabelFontSize
+        {
+            get { return labelFontSize; }
+            set
+            {
+                labelFontSize = value;
+                EnableCheckBox.FontSize = labelFontSize;
+                FirstLabel.FontSize = labelFontSize;
+                SecondLabel.FontSize = LabelFontSize;
+                ThirdLabel.FontSize = LabelFontSize;
+            }
+        }
+        public int FirstTextBoxWidth
+        {
+            get { return firstTextBoxWidth; }
+            set
+            {
+                firstTextBoxWidth = value;
+                FirstTextBox.Width = firstTextBoxWidth;
+            }
+        }
+        public int SecondTextBoxWidth
+        {
+            get { return secondTextBoxWidth; }
+            set
+            {
+                secondTextBoxWidth = value;
+                SecondTextBox.Width = secondTextBoxWidth;
+            }
+        }
+        public int ThirdTextBoxWidth
+        {
+            get { return thirdTextBoxWidth; }
+            set
+            {
+                thirdTextBoxWidth = value;
+                ThirdTextBox.Width = thirdTextBoxWidth;
+            }
+        }
+        public int CheckBoxSize
+        {
+            get { return checkBoxSize; }
+            set
+            {
+                checkBoxSize = value;
+                CheckSize.Height = checkBoxSize;
+            }
+        }
+        public Brush LabelColor
+        {
+            get { return labelColor; }
+            set
+            {
+                labelColor = value;
+                EnableCheckBox.Foreground = labelColor;
+                FirstLabel.Foreground = labelColor;
+                SecondLabel.Foreground = labelColor;
+                ThirdLabel.Foreground = labelColor;
+            }
+        }
+        public string CheckBoxContent
+        {
+            get { return checkBoxContent; }
+            set
+            {
+                checkBoxContent = value;
+                EnableCheckBox.Content = checkBoxContent;
+            }
+        }
+        public string FirstLabelContent
+        {
+            get { return firstLabelContent; }
+            set
+            {
+                firstLabelContent = value;
+                FirstLabel.Content = firstLabelContent;
+            }
+        }
+        public string SecondLabelContent
+        {
+            get { return secondLabelContent; }
+            set
+            {
+                secondLabelContent = value;
+                SecondLabel.Content = secondLabelContent;
+            }
+        }
+        public string ThirdLabelContent
+        {
+            get { return thirdLabelContent; }
+            set
+            {
+                thirdLabelContent = value;
+                ThirdLabel.Content = thirdLabelContent;
+            }
+        }
+        public NameTextEntryCheckBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GM_Toolbox/GM_Toolbox/NPCGenerator.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GM_Toolbox
+{
+    /// <summary>
+    /// Interaction logic for NPCGenerator.xaml
+    /// </summary>
+    public partial class NPCGenerator : Window
+    {
+        public NPCGenerator()
+        {
+            InitializeComponent();
+        }
+    }
+}
