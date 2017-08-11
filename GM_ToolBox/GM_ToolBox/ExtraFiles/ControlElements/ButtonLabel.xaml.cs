36ae7f067443103ef404181c813df40c4c57014e
JNash7298 <JNash@student.neumont.edu>
2017-08-10 19:25:58 -0600

    This had better be fixed and commit

---
+++ GM_ToolBox/GM_ToolBox/ExtraFiles/ControlElements/ButtonLabel.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GM_Toolbox.ControlElements
+{
+    /// <summary>
+    /// A control element that provides access to a labeled button.
+    /// </summary>
+    public partial class ButtonLabel : UserControl
+    {
+        private int labeledButtonWidth;
+        private int labeledButtonHeight;
+        private Brush labelColor;
+        private string labelContent;
+        public int LabeledButtonWidth
+        {
+            get { return labeledButtonWidth; }
+            set
+            {
+                labeledButtonWidth = value;
+                LabeledButton.Width = labeledButtonWidth;
+            }
+        }
+        public int LabeledButtonHeight
+        {
+            get { return labeledButtonHeight; }
+            set
+            {
+                labeledButtonHeight = value;
+                LabeledButton.Height = labeledButtonHeight;
+            }
+        }
+        public Brush LabelColor
+        {
+            get { return labelColor; }
+            set
+            {
+                labelColor = value;
+                LabeledButtonLabel.Foreground = labelColor;
+            }
+        }
+        public string LabelContent
+        {
+            get { return labelContent; }
+            set
+            {
+                labelContent = value;
+                LabeledButtonLabel.Content = labelContent;
+            }
+        }
+        public ButtonLabel()
+        {
+            InitializeComponent();
+        }
+    }
+}
