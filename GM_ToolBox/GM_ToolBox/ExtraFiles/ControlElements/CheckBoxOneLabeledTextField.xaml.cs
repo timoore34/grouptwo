c228fa95bc5d57f77dfd084895a9528c55ed3c98
JNash7298 <JNash@student.neumont.edu>
2017-08-14 09:29:05 -0600

    Added a little more to NPC need to work more on it though

---
+++ GM_ToolBox/GM_ToolBox/ExtraFiles/ControlElements/CheckBoxOneLabeledTextField.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GM_ToolBox.ExtraFiles.ControlElements
+{
+    /// <summary>
+    /// Interaction logic for CheckBoxOneLabeledTextField.xaml
+    /// </summary>
+    public partial class CheckBoxOneLabeledTextField : UserControl
+    {
+        private int LabelFontSize;
+        private int TextBoxWidth;
+        private string LabelContent;
+        private string CheckBoxContent;
+        private Brush LabelColor;
+        public CheckBoxOneLabeledTextField()
+        {
+            InitializeComponent();
+        }
+    }
+}
