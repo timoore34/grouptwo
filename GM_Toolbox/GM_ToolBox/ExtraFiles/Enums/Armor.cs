772a5878d32b3a7a375c4509d9959a92bfe70eed
JNash7298 <JNash@student.neumont.edu>
2017-08-17 17:51:35 -0600

    Redone a few things and more of the race data is coming alone

---
+++ GM_Toolbox/GM_ToolBox/ExtraFiles/Enums/Armor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GM_ToolBox.ExtraFiles.Enums
+{
+    public enum Armor
+    {
+        Light,
+        Medium,
+        Heavy,
+        Shield
+    }
+}
---
+++ GM_Toolbox/GM_ToolBox/ExtraFiles/Enums/Languages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GM_ToolBox.ExtraFiles.Enums
+{
+    public enum Languages
+    {
+        Common,
+        Dwarvish,
+        Elvish,
+        Giant,
+        Gnomish,
+        Goblin,
+        Halfling,
+        Orc,
+        Abyssal,
+        Celestial,
+        Draconic,
+        Deep_Speech,
+        Infernal,
+        Primordial,
+        Sylvan,
+        Undercommon
+    }
+}
