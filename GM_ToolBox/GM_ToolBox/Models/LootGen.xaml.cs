2a2ffe6c1607d1e0cf93c1a7f17f122b0c8c0983
EStansell <30707699+EStansell@users.noreply.github.com>
2017-08-31 17:24:36 -0600

    Adds a nonfunctional import bar for custom items.
    Not implemented.

--- GM_ToolBox/GM_ToolBox/Models/LootGen.xaml.cs
+++ GM_ToolBox/GM_ToolBox/Models/LootGen.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using GM_ToolBox.ExtraFiles.Parsing;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,12 +17,26 @@ using System.Windows.Shapes;
 
 namespace GM_Toolbox.Models
 {
-
     /// <summary>
     /// Interaction logic for LootGen.xaml
     /// </summary>
     public partial class LootGen : Window
     {
+        private List<Item> item = new List<Item>()
+        {
+                new Item("Gold", 25, false, "Here's 25 gold. Try investing into your future a bit more."),
+                new Item("Gold", 50, false, "Here's 50 gold. spend it wisely"),
+                new Item("Gold", 150, false, "Here's 150 gold. Treat yourself to a visit at your local bar and go crazy."),
+                new Item("Fancy Looking Hat", 65, false, "The quality of this hat is superb. you recon it could save your life"
+                    + "from a sword swing.\n+1 to your AC"),
+                new Item("Twig", 0, false, "I dont even know why you want this in your inventory"),
+                new Item("Broadsword", 175, true, "This sword is what is commonly carried around by knights of your local kingdom. As you inspect"
+                +" your newfound blade, you notice the crest on the hilt of the sword is glowing slightly. If the blade found you to be knightly like its predesesor, it"
+                +" would probably grant you even greater strength\n+1 damage to Knights serving in the military"),
+                new Item("Apple", 25, false, "A delicious fruit or a light snack. Whichever you prefer, you would feel envigorated after ingesting.\nRecover 3HP"),
+                new Item("Ex-Calibur", 300, true, "The son of excalibur who hold more power than even the gods. This sword contains sentience and never "
+                    + "claimed a master worthy enough to wield him. Hopefully your party is prepared for him")
+        };
         bool[] Rarities = new bool[10];
         int sliderNumber = 1;
 
@@ -36,20 +53,7 @@ namespace GM_Toolbox.Models
         }
         private void GenerateButton_Click(object sender, RoutedEventArgs e)
         {
-            GM_ToolBox.ExtraFiles.Parsing.Item[] items = {
-            new GM_ToolBox.ExtraFiles.Parsing.Item("Gold", 25, false, "Here's 25 gold. Try investing into your future a bit more."),
-            new GM_ToolBox.ExtraFiles.Parsing.Item("Gold", 50, false, "Here's 50 gold. spend it wisely"),
-            new GM_ToolBox.ExtraFiles.Parsing.Item("Gold", 150, false, "Here's 150 gold. Treat yourself to a visit at your local bar and go crazy."),
-            new GM_ToolBox.ExtraFiles.Parsing.Item("Fancy Looking Hat", 65, false, "The quality of this hat is superb. you recon it could save your life"
-                + "from a sword swing.\n+1 to your AC"),
-            new GM_ToolBox.ExtraFiles.Parsing.Item("Twig", 0, false, "I dont even know why you want this in your inventory"),
-            new GM_ToolBox.ExtraFiles.Parsing.Item("Broadsword", 175, true, "This sword is what is commonly carried around by knights of your local kingdom. As you inspect"
-            +" your newfound blade, you notice the crest on the hilt of the sword is glowing slightly. If the blade found you to be knightly like its predesesor, it"
-            +" would probably grant you even greater strength\n+1 damage to Knights serving in the military"),
-            new GM_ToolBox.ExtraFiles.Parsing.Item("Apple", 25, false, "A delicious fruit or a light snack. Whichever you prefer, you would feel envigorated after ingesting.\nRecover 3HP"),
-            new GM_ToolBox.ExtraFiles.Parsing.Item("Ex-Calibur", 300, true, "The son of excalibur who hold more power than even the gods. This sword contains sentience and never "
-                + "claimed a master worthy enough to wield him. Hopefully your party is prepared for him")
-        };
+
 
             Random randalf = new Random();
             string returnToUser = "";
@@ -61,11 +65,11 @@ namespace GM_Toolbox.Models
                 }
                 for (int i = 0; i < sliderNumber; i++)
                 {
-                    returnToUser += items[randalf.Next(0, 8)].toString() + "\n\n";
+                    returnToUser += item[randalf.Next(0, 8)].toString() + "\n\n";
                 }
                 ResultTextBlock.Text = returnToUser;
             }
-            catch(Exception)
+            catch (Exception)
             {
                 ResultTextBlock.Text = "Value in \"Total Items\" box was found no to have a number in it. ";
             }
@@ -76,7 +80,7 @@ namespace GM_Toolbox.Models
 
         private void SelectAll_Checked(object sender, RoutedEventArgs e)
         {
-            for(int i = 1; i < Rarities.Length; i++)
+            for (int i = 1; i < Rarities.Length; i++)
             {
                 Rarities[i] = true;
             }
@@ -96,6 +100,21 @@ namespace GM_Toolbox.Models
         private void slider_GiveFeedback(object sender, GiveFeedbackEventArgs e)
         {
             sliderNumber = int.Parse(sender.ToString());
+        }
+
+        private void inputItems_Click(object sender, RoutedEventArgs e)
+        {
+
+            FileStream fs = new FileStream("", FileMode.Open);
+            BinaryFormatter formatter = new BinaryFormatter();
+            //List<Item> dataList
+            item = (List<Item>)formatter.Deserialize(fs);
+            fs.Close();
+
+            //to serialize the data just do the same thing but
+            formatter.Serialize(fs, item);
+            //This way you have all the data stored in an List that is easly assessable and changable
+            
         }
     }
 }
